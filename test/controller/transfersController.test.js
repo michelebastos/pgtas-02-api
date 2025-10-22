a53834fec08fb31ea910ca1c546686acae592271
Michele Bastos <michele.bastosqa@gmail.com>
2025-10-22 20:20:54 -0300

    First commit

---
+++ test/controller/transfersController.test.js
@@ -0,0 +1,51 @@
+
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../../app');
+
+describe('Transfer Controller', () => {
+describe('POST /transfers', () => {
+  it('Quando informo remetente e destinatário inexistente, deve retorna 404', async () => {
+    const resposta = await request(app)
+      .post('/transfers')
+      .send({
+        fromId: "julio",
+        toId: "Maria",
+        amount: 100
+        });
+    expect(resposta.status).to.equal(404);
+    expect(resposta.body).to.have.property('error', 'origin user not found');
+  });
+
+   it('Quando informo valor=0  retorna 400', async () => {
+    const resposta = await request(app)
+      .post('/transfers')
+      .send({
+        fromId: "julio",
+        toId: "Maria",
+        amount: 0
+      });
+
+    expect(resposta.status).to.equal(400);
+    expect(resposta.body).to.have.property('error', 'fromId, toId and amount (>0) are required');
+    
+  });
+
+  it('Quando não informo destinatário e rementente retorna 400', async () => {
+    const resposta = await request(app)
+      .post('/transfers')
+      .send({
+        fromId: "",
+        toId: "",
+        amount: 100
+      });
+
+    expect(resposta.status).to.equal(400);
+    expect(resposta.body).to.have.property('error', 'fromId, toId and amount (>0) are required');
+    
+  });
+
+});
+});
---
+++ test/controller/usersController.test.js
@@ -0,0 +1,47 @@
+
+// Bibliotecas
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../../app');
+
+describe('Users Controller', () => {
+    describe('POST /Users', () => {
+        it('Quando informo dados válidos para usuário favorecido, deve retorna 201', async () => {
+            const resposta = await request(app)
+            .post('/users/register')
+            .send({
+                name: "Noah",
+                email: "noah@gmail.com",
+                password: "123",
+                favored: true,
+                balance: 1000
+            });
+        expect(resposta.status).to.equal(201);
+
+        it('Quando informo dados já exisistentes, deve retorna 201', async () => {
+            const resposta = await request(app)
+            .post('/users/register')
+            .send({
+                name: "Ana",
+                email: "anassss",
+                password: "",
+                favored: true,
+                balance: 0
+            });
+        expect(resposta.status).to.equal(409);
+        expect(resposta.body).to.have.property('error', 'user already exists');
+    });
+
+    describe('GET /Users', () => {
+        it('Deve retornar todos os usuários com status 200', async () => {
+            const resposta = await request(app)
+            .get('/users/')
+            expect(resposta.status).to.equal(200);
+
+});
+});
+});
+});
+});
